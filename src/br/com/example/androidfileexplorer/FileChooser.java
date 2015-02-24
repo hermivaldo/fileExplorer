3d2fbe14b44420a0f005b961c6dff956c2ebf8cd
hermivaldo <hermivaldo.braga@gmail.com>
2015-02-23 21:49:05 -0300

    Alteração para carregar arquivos externos (imagens para dentro da tela do dispositivo).

--- src/br/com/example/androidfileexplorer/FileChooser.java
+++ src/br/com/example/androidfileexplorer/FileChooser.java
@@ -87,7 +87,7 @@ public class FileChooser extends ListActivity{
         //Toast.makeText(this, "Folder Clicked: "+ currentDir, Toast.LENGTH_SHORT).show();
         Intent intent = new Intent();
         intent.putExtra("GetPath",currentDir.toString());
-        intent.putExtra("GetFileName",o.getName());
+        intent.putExtra("GetFileName",o.getPath());
         setResult(RESULT_OK, intent);
         finish();
     }
--- src/br/com/example/androidfileexplorer/MainActivity.java
+++ src/br/com/example/androidfileexplorer/MainActivity.java
@@ -1,21 +1,34 @@
 package br.com.example.androidfileexplorer;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class MainActivity extends Activity {
 
     private static final int REQUEST_PATH = 1;
     String curFileName;    
     EditText edittext;
+    ImageView mImage;
+	private ViewGroup mV;
+    
     @Override
 public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    mV = (ViewGroup) findViewById(R.id.tela);
+    
+    mImage = (ImageView) findViewById(R.id.imageView1);
     edittext = (EditText)findViewById(R.id.editText);
 }
 
@@ -28,8 +41,14 @@ protected void onActivityResult(int requestCode, int resultCode, Intent data){
     // See which child activity is calling us back.
     if (requestCode == REQUEST_PATH){
             if (resultCode == RESULT_OK) {
+            	
                     curFileName = data.getStringExtra("GetFileName");
+                    Bitmap map = BitmapFactory.decodeFile(curFileName);
+                    mImage.setImageBitmap(map);
                     edittext.setText(curFileName);
+                    this.mV.invalidate();
+                    this.setContentView(mV);
+                    
             }
      }
 }
